e808d51178afa7ad4c36b030f0439b68cecd2de8
chihuahua <chihuahua@babydoge.com>
2025-02-23 13:10:36 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { NCarousel } from "naive-ui";
+</script>
+
+<template>
+  <div class="w-full flex flex-col max-w-[1240px] mx-auto">
+    <div class="flex w-full items-center justify-center pt-[64px] pb-[32px]">
+      <img
+        src="/img/froggy.png"
+        alt="froggy-png"
+        class="w-[200px] h-auto shrink-0"
+      />
+      <h1 class="relative z-10 left-[-1em] text-6xl">Froggy one</h1>
+    </div>
+
+    <p>Здесь описание того, куда я хожу, и что я узнал новенького за эти дни</p>
+    <p>
+      Да, собственно есть, инстаграм или телеграмм, но это все же не то, что бы
+      я хотел для себя
+    </p>
+    <p>
+      Всякие вещи, которые я хочу, я буду размещать тут, т.к мне дико этого
+      хочется, иметь реально свое занятие и свои страсти
+    </p>
+
+    <section>
+      <h2>Дата 22.02.2025</h2>
+
+      <NCarousel show-arrow class="rounded-[24px] w-full max-w-[300px]">
+        <img class="w-full h-[200px] object-cover" src="/img/content/1.png" />
+        <img class="w-full h-[200px] object-cover" src="/img/content/2.png" />
+        <img class="w-full h-[200px] object-cover" src="/img/content/3.png" />
+        <img class="w-full h-[200px] object-cover" src="/img/content/4.png" />
+      </NCarousel>
+
+      <p>
+        В этот день я ходил на выставку водного человека. Не знал точно, что
+        меня там ждет, вообще я искал выставки одежды, чтобы посмотреть на
+        интересные штуки и подобная выставка меня смущала.
+      </p>
+
+      <p>
+        Там было очень много дайверов, разных программ дайвинга, продажи
+        костюмов, техники, средств для ушей, массаж какой-то странный, выставка
+        картин, игрушки, Издательство "Самокат", продажа устриц и арктической
+        рыбы всякой, это было круто
+      </p>
+
+      <p>
+        Там было как раз чем наградить себя за прошедшую неделю, я купил
+        копченого муксуна, две книжки в издательстве самокат, алтайского варенья
+        без сахара: с черникой, брусникой и клюквой.
+        И купил себе желтую кофту из Captain's Morgan.
+        Конечно, до конца не уверен, нравится мне она или нет, но с чайкой были только женские, что грустно
+      </p>
+    </section>
+
+
+    <section>
+      <h2>Дата 22.02.2025</h2>
+
+      <NCarousel show-arrow class="rounded-[24px] w-full max-w-[300px]">
+        <img class="w-full h-[200px] object-cover" src="/img/content/1.png" />
+        <img class="w-full h-[200px] object-cover" src="/img/content/2.png" />
+        <img class="w-full h-[200px] object-cover" src="/img/content/3.png" />
+        <img class="w-full h-[200px] object-cover" src="/img/content/4.png" />
+      </NCarousel>
+
+      <p>
+        Приехали мои жабки!!!
+      </p>
+    </section>
+  </div>
+</template>
+
+<style scoped></style>
